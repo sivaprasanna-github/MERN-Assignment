b52411143c5f48955938e5cacfe5a125034e3765
Siva Prasanna Gorla <sivaprasannagorla@gmail.com>
2026-06-21 22:20:02 +0530

    CORS

--- backend/server.js
+++ backend/server.js
@@ -19,7 +19,8 @@ const allowedOrigins = [
   "http://localhost:5173",
   "http://localhost:3000",
   "http://127.0.0.1:5173",
-  "http://127.0.0.1:3000"
+  "http://127.0.0.1:3000",
+  "https://rengy-sivaprasanna.netlify.app"
 ];
 
 if (process.env.CLIENT_ORIGIN) {
